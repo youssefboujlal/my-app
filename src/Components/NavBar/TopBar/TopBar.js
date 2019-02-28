ae1cff2f8a7821ad07a00a868d2adab03f96199c
youssefboujlal <youssef.boujlal@gmail.com>
2019-03-01 00:07:47 +0100

    add navBar

--- src/Components/NavBar/TopBar/TopBar.js
+++ src/Components/NavBar/TopBar/TopBar.js
@@ -1,41 +1,81 @@
 import React, { Component } from 'react';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-//import RightBar from '../RightBar/RighBar';
+
+import Breadcrumb from '../Breadcrumbs/Breadcrumbs'
+
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem ,Spinner } from 'reactstrap';
+
+
 
 class TopBar extends Component {
-  
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
     render() {
-         
+      return (
+       
 
-
-      return   (
-        <Router>
-    <div>
-      <ul>
-        <li>
-          <Link to="/">Homedd</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/topics">Topics</Link>
-        </li>
-      </ul>
-
-      <hr />
-
-    
-    </div>
-  </Router>
-      )
-
-    
+        <div>
+      
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">Test</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/">Page1</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">Page2</NavLink>
+              </NavItem>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  Options
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>
+                    Option 1
+                  </DropdownItem>
+                  <DropdownItem>
+                    Option 2
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>
+                    Reset
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+          </Collapse>
+        </Navbar>
+        <Breadcrumb />
+      </div>
+      
+      );
       
     }
+    
   }
+  
   
 export default TopBar;
